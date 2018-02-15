c18b36cc1f268ffd62783e2d62d1adcc1fff356a
hurricaneronron <vprzybyl@gmail.com>
2018-02-14 22:30:48 -0800

    built out framework and style

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready( function ( ) {
+
+function gameStart( ) {
+  $(".question").html("Test your Harry Potter knowledge!")
+  $(".answer-choices").html("<button type='button' class='btn btn-secondary btn-lg begin'>Begin</button>")
+}
+gameStart ( )
+
+// question function
+// timer
+// question
+// clickable answer choices
+
+
+})
