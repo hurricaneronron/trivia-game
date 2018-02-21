908de8cf7a34c736e24f2a12e3023005f62109b3
hurricaneronron <vprzybyl@gmail.com>
2018-02-20 17:11:22 -0700

    working game without Try Again and Timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,58 +4,62 @@ $(document).ready( function ( ) {
 var questions = [
    question1 = {
     question : "What is Albus Dumbledore's full name?",
-    rightAns : "What is Albus Dumbledore's full name?",
-    answer1 : "Albus Patrick William Bob Dumbledore",
-    answer2 : "Albus Brian Percival Wulfric Dumbledore",
-    answer3 : "Albus William Bob Patrick Dumbledore",
+    A : "<button class='correct'>Albus Percival Wulfric Brian Dumbledore</button>",
+    B : "<button class='incorrect'>Albus Patrick William Bob Dumbledore</button>",
+    C : "<button class='incorrect'>Albus Brian Percival Wulfric Dumbledore</button>",
+    D : "<button class='incorrect'>Albus William Bob Patrick Dumbledore</button>",
     gif : "<img src='./assets/images/dumbledore.gif'>",
   },
   
   question2 = {
     question : "Which two characters share a Patronus?",
-    answer1 : "Ron Weasley and Neville Longbottom",
-    answer2 : "Harry Potter and James Potter",
-    rightAns : "Severus Snape and Lily Potter",
-    answer3 : "Dean Thomas and Ginny Weasley",
+    A : "<button class='incorrect'>Ron Weasley and Neville Longbottom</button>",
+    B : "<button class='incorrect'>Harry Potter and James Potter</button>",
+    C : "<button class='correct'>Severus Snape and Lily Potter</button>",
+    D : "<button class='incorrect'>Dean Thomas and Ginny Weasley</button>",
     gif : "<img src='./assets/images/patronus.gif'>",
   },
   
   question3 = {
     question : "When Harry and Ron visit the Slytherin common room in disguise in The Chamber of Secrets, what is the password used to get inside?",
-    answer1 : "Salazar",
-    rightAns : "Pure-blood",
-    answer2 : "Parselmouth",
-    answer3 : "Draco dormiens nunquam titillandus",
+    A : "<button class='incorrect'>Salazar</button>",
+    B : "<button class='correct'>Pure-blood</button>",
+    C : "<button class='incorrect'>Parselmouth</button>",
+    D : "<button class='incorrect'>Draco dormiens nunquam titillandus</button>",
     gif : "<img src='./assets/images/polyjuice.gif'>",
   },
   
   question4 = {
     question : "James Potter, Sirius Black, and Peter Pettigrew are the only known unregistered Animagi - until who comes along?",
-    answer1 : "Aberforth Dumbledore",
-    rightAns : "Rita Skeeter",
-    answer2 : "Nymphadora Tonks",
-    answer3 : "Ludo Bagman",
+    A : "<button class='incorrect'>Aberforth Dumbledore</button>",
+    B : "<button class='correct'>Rita Skeeter</button>",
+    C : "<button class='incorrect'>Nymphadora Tonks</button>",
+    D : "<button class='incorrect'>Ludo Bagman</button>",
     gif : "<img src='./assets/images/rita-skeeter.gif'>",
   },
 
   question5 = {
     question : "In The Prisoner of Azkaban, which secret passage out of Hogwarts leads Harry to the cellar of Honeydukes?",
-    rightAns : "Behind the statue of the one-eyed witch",
-    answer1 : "Behind the statue of Gregory the Smarmy",
-    answer2 : "Inside the Vanishing Cabinet",
-    answer3 : "Beneath the Whomping Willow",
+    A : "<button class='correct'>Behind the statue of the one-eyed witch</button>",
+    B : "<button class='incorrect'>Behind the statue of Gregory the Smarmy</button>",
+    C : "<button class='incorrect'>Inside the Vanishing Cabinet</button>",
+    D : "<button class='incorrect'>Beneath the Whomping Willow</button>",
     gif : "<img src='./assets/images/map.gif'>",
   },
 
   question6 = {
     question : "In The Deathly Hallows, what are the names of the three Peverell brothers?",
-    rightAns : "Antioch, Cadmus, and Ignotus",
-    answer1 : "Antioch, Crydia, and Ignatius",
-    answer2 : "Aberforth, Cadmus, and Ignotus",
-    answer3 : "Antioch, Cadmus, and Ibrahim",
+    A : "<button class='correct'>Antioch, Cadmus, and Ignotus</button>",
+    B : "<button class='incorrect'>Antioch, Crydia, and Ignatius</button>",
+    C : "<button class='incorrect'>Aberforth, Cadmus, and Ignotus</button>",
+    D : "<button class='incorrect'>Antioch, Cadmus, and Ibrahim</button>",
     gif : "<img src='./assets/images/deathly-hallows.gif'>",
   },
 ]
+
+var questionNum = -1
+var questionsCorrect = 0
+var questionsIncorrect = 0
 
 // load page and begin game
 function gameStart( ) {
@@ -65,21 +69,50 @@ function gameStart( ) {
 gameStart ( )
 
 $(".begin").on("click", function ( ) {
-  $(".question").html(question1.question)
-  $(".answer-choices").append("<p>"+question1.rightAns+"</p>")
-  $(".answer-choices").append("<p>"+question1.answer1+"</p>")
-  $(".answer-choices").append("<p>"+question1.answer2+"</p>")
-  $(".answer-choices").append("<p>"+question1.answer3+"</p>")
   $(".begin").hide()
-  $(".begin").off()
+  // $(".begin").off()
+  // display timer
+  gamePlay( )
+
+  // game play
+  function gamePlay( ) {
+    questionNum++
+    $(".question").html(questions[questionNum].question)
+    $(".answer-choices").append("<p>"+questions[questionNum].A+"</p>")
+    $(".answer-choices").append("<p>"+questions[questionNum].B+"</p>")
+    $(".answer-choices").append("<p>"+questions[questionNum].C+"</p>")
+    $(".answer-choices").append("<p>"+questions[questionNum].D+"</p>")
+    $(".correct").on("click", function ( ){
+      questionsCorrect++
+      $(".question").html("Correct!")
+      $(".answer-choices").html(questions[questionNum].gif)
+      var wait = setTimeout( function( ){
+        $(".answer-choices").empty( )
+        if(questionNum === questions.length-1) {
+          $(".question").empty( )
+          $(".answer-choices").html("<h3>End of Game</h3><h4>Correct Answers: "+questionsCorrect+"</h4><h4>Incorrect Answers: "+questionsIncorrect+"</h4><p><button type='button' class='btn btn-secondary btn-lg begin'>Try Again</button><p>")
+        }
+        else {
+          gamePlay( )
+        }
+      }, 2000)
+    })
+    $(".incorrect").on("click", function ( ){
+      questionsIncorrect++
+      $(".question").html("Incorrect. The correct answer is: "+$(".correct").text( )+"")
+      $(".answer-choices").html(questions[questionNum].gif)
+      var wait = setTimeout( function( ){
+        $(".answer-choices").empty( )
+        if(questionNum === questions.length-1) {
+          $(".question").empty( )
+          $(".answer-choices").html("<h3>End of Game</h3><h4>Correct Answers: "+questionsCorrect+"</h4><h4>Incorrect Answers: "+questionsIncorrect+"</h4><p><button type='button' class='btn btn-secondary btn-lg begin'>Try Again</button><p>")
+        }
+        else {
+          gamePlay( )
+        }
+      }, 2000)
+    })
+    
+  }
 })
-
-// display timer question answer choices
-// clickable answer choices
-  // if correct congratulate them and show gif
-  // $(".question").html("Correct!")
-  // $(".answer-choices").html("<img src='")
-  // if incorrect tell them the correct answer and show gif
-
-
 })
